e83d38aee4a1e7ec6907415f37113ad995c84894
Lucas Gouveia Belon <lucasgouveiabelon@gmail.com>
2023-05-22 01:46:29 -0300

    Estou parando este projeto por enquanto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ShellScript
 I'll take notes based on a book about how to write good scripts. Let's see what shellscripts has to offer
+
+## This Repo is stopped bc shellscript is weird.
--- appendix_A/04_if-for-while.sh
+++ appendix_A/04_if-for-while.sh
@@ -18,9 +18,57 @@ VAR=$1 #Receives the first argument
 
 if test $VAR -gt 10
 then
-    echo "Is bigger than 10"
+    echo "$VAR Is bigger than 10"
 else
-    echo "Isn't bigger than 10"
+    echo "$VAR Isn't bigger than 10"
 fi
+
+# Another way is to use square brackets on the
+# if statements. There is a difference between
+# these two, better search later.
+
+if [ $VAR -gt 10 ]
+then
+    echo "$VAR Is bigger than 10"
+else
+    echo "$VAR Isn't bigger than 10"
+fi
+
+# The spaces around [] are necessary.
+
+# While testing "is file"
+while test -f /tmp/lock
+do
+    echo "Script locked"
+    sleep 1
+done
+
+for number in one two three
+do
+    echo "Counting... $number"
+done
+
+for step in $(seq 10)
+do
+    echo "Counting... $step"
+done
+
+i=0
+while test $i -le 10
+do
+    i=$((i+1))
+    echo "counting $i"
+done
+
+while :
+do
+    if test -f /tmp/lock
+    then
+        echo "Aguardando..."
+        sleep 1
+    else
+        break
+    fi
+done
 
 
